7c631677bac6f796c41b269ca9fb028eba49b9cc
Angel Solis <25460338@colima.tecnm.mx>
2025-11-20 11:47:32 -0600

    2do commit version 1

--- src/script.py
+++ src/script.py
@@ -10,7 +10,7 @@ ventana.title("Mi primera ventana")
 label = tk.Label(ventana, text="Presiona el botón para ver un mensaje")
 label.pack(pady=10)
 
-boton = tk.Button(ventana, text="Haz click aqui", command=mostrarMensaje)
+boton = tk.Button(ventana, text="Haz click aca", command=mostrarMensaje)
 boton.pack(pady=10)
 
 ventana.mainloop()
