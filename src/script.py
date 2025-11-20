2cc9b40f8a24398bdb0fdfeb4c080439c4c4e12f
Angel Solis <25460338@colima.tecnm.mx>
2025-11-20 11:44:46 -0600

    Segundo commit: Ejemplo Tkinter

---
+++ src/script.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+from tkinter import messagebox
+
+def mostrarMensaje():
+    messagebox.showinfo("Aviso", "Botón presionado!")
+
+ventana = tk.Tk()
+ventana.title("Mi primera ventana")
+
+label = tk.Label(ventana, text="Presiona el botón para ver un mensaje")
+label.pack(pady=10)
+
+boton = tk.Button(ventana, text="Haz click aqui", command=mostrarMensaje)
+boton.pack(pady=10)
+
+ventana.mainloop()
